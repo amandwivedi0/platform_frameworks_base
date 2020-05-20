e8aaabe5ecaf0c31b682de211f2dcfb59bda1ab5
SuperDroidBond <superdroidbond@yahoo.com>
2020-05-21 01:08:08 +0530

    Random Qs Tint: tint active QS Tiles with random colors [1/2]
    Xtended the QS Tint Options to -:
      1) 1 = Random Color Tint (Default)
      2) 2 = Accent Based Tint
      3) 3 = Off
    NAD: Adapt for our code without qs themes
    
    Special Thanks to Potato ROM for idea -
        https://github.com/PotatoProject/frameworks_base/commit/8b30ef966ddb8f28763998e3aabf69e62b7351a4
    
    Change-Id: I388c68c69bc5da791e617dc87e46c077805a9052
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>
    Signed-off-by: Genkzsz11 <genkzsz11@nusantaradevs.org>
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
@@ -31,8 +31,10 @@ import android.annotation.CallSuper;
 import android.annotation.NonNull;
 import android.app.ActivityManager;
 import android.content.res.ColorUtils;
+import android.content.res.Configuration;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.metrics.LogMaker;
 import android.os.Handler;
@@ -78,6 +80,7 @@ import com.android.systemui.qs.logging.QSLogger;
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Base quick-settings tile, extend this to create a new tile.
@@ -527,8 +530,9 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
     public abstract CharSequence getTileLabel();
 
     public static int getColorForState(Context context, int state) {
-        boolean setQsUseNewTint = Settings.System.getIntForUser(context.getContentResolver(),
-                Settings.System.QS_PANEL_BG_USE_NEW_TINT, 1, UserHandle.USER_CURRENT) == 1;
+        int setQsUseNewTint = Settings.System.getIntForUser(context.getContentResolver(),
+                    Settings.System.QS_PANEL_BG_USE_NEW_TINT, 0, UserHandle.USER_CURRENT);
+
         switch (state) {
             case Tile.STATE_UNAVAILABLE:
                 return Utils.getDisabled(context,
@@ -540,7 +544,9 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
                     return Utils.getColorAttrDefaultColor(context, android.R.attr.textColorSecondary);
                 }
             case Tile.STATE_ACTIVE:
-                if (setQsUseNewTint) {
+                if (setQsUseNewTint == 1) {
+                    return ColorUtils.genRandomAccentColor(isThemeDark(context));
+                } else if (setQsUseNewTint == 2) {
                     return Utils.getColorAttrDefaultColor(context, android.R.attr.colorAccent);
                 } else {
                     return Utils.getColorAttrDefaultColor(context, android.R.attr.colorPrimary);
@@ -753,4 +759,16 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
         pw.println(this.getClass().getSimpleName() + ":");
         pw.print("    "); pw.println(getState().toString());
     }
+
+
+    private static Boolean isThemeDark(Context context) {
+        switch (context.getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK) {
+            case Configuration.UI_MODE_NIGHT_YES:
+              return true;
+            case Configuration.UI_MODE_NIGHT_NO:
+              return false;
+            default:
+              return false;
+         }
+     }
 }
